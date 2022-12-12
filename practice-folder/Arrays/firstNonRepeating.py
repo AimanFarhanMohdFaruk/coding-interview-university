9bdb4e6815236618bc106f235dc34776e65b57b1
Aiman Farhan <aiman.faruk@surialabs.com>
2022-12-12 13:15:17 +0800

    Add practice folder

---
+++ practice-folder/Arrays/firstNonRepeating.py
@@ -0,0 +1,17 @@
+
+def FirstNonRepeating(str):
+    char_order = []
+    count = {}
+
+    for char in str:
+      if char in count:
+        count[char] += 1
+      else:
+        count[char] = 1
+        char_order.append(char)
+    for char in char_order:
+        if count[char] == 1:
+          return char
+    return None
+
+print("First Non-Repeating Character = ",FirstNonRepeating('thisisit'))

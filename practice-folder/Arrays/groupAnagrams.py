12eff6dc2c849bc1004f5763d88e5d41d4106b55
Aiman Farhan <aiman.faruk@surialabs.com>
2023-01-30 22:01:30 +0800

    Longest sequence

--- practice-folder/Arrays/groupAnagrams.py
+++ practice-folder/Arrays/groupAnagrams.py
@@ -8,10 +8,19 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
     count = [0] * 26 # a....z each lowercase char
     for c in s:
       # map character (a) to 0, and the rest to the following.
+      # why minus ord("a") ? because thats the starting point.
         count[ord(c) - ord("a")] += 1
+    # tuple because list cannot be keys in Python
+    # then you, add the string to the answers dictinionary.
     ans[tuple(count)].append(s)
   return ans.values()
 
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 
 # O(m * n)
+
+
+# Notes:
+# The hashmap keys, are counts of each character,
+# Meaning, for 'eat' and 'tea', the character counts would be the same,
+# hence, they are grouped the same way
--- practice-folder/Arrays/longestSequence.py
+++ practice-folder/Arrays/longestSequence.py
@@ -11,25 +11,33 @@ def longestSequence(array):
 
 
 def longestSequenceTwo(array):
-  numbers = set(array)
+  nums = set(array)
   longest = 0
 
-  for num in numbers:
-    if(num - 1) not in numbers:
+  for num in nums:
+    if (num - 1) not in nums: #dont check if its not the start of a sequence
       length = 1
-      while (num + length) in numbers:
-        length += 1
-      longest = max(length, longest)
+    while (num + length) in nums: #basic increment, both to get the following number and the current length of sequence
+      length += 1
+      longest = max(longest, length)
   return longest
+      
 
 print(longestSequence([1,2,3,100,200]))
 
-print(longestSequenceTwo([1,2,3,100,200]))
+print(longestSequenceTwo([1,2,3,4,100, 101, 102,103,104,105,200]))
 
-# pattern:
-# remove all duplicates using set
-# for each num in the list:
-#   set the temporary sequence count to 1
-# while num + 1, is in numbers,
-# add to the temporary sequence count and add the num
-# the length is the max between the current lenght and temporary
+# pointers
+# What makes a sequence?
+  # it has a start and end,
+  # the start value does not have a left neighbour
+
+# using this logic
+# we can deduce using the following:
+  # for each number in the array
+    # check if it has a left neighbour
+      # if it doesnt, means its the start of a sequence
+      # initiate the lenght of 1
+      # while it has a right neighbour (num + length) that is in the array of numbers, increase the length
+      # longest = max(length(which is the current running count), longest)
+  # return longest
